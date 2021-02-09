9e7b5deb5fbc24e2ac926ffb76716561135185f0
Noman1170 <77617625+Noman1170@users.noreply.github.com>
2021-02-09 03:18:32 -0800

    feb 09.02

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-7 + 11 - 4 + 23;
-console.log(7 + 11 - 4 + 23);
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20){
+            $('.navbar').addClass("sticky");
+        
+        }else{
+            
+        }
+    })
+})
