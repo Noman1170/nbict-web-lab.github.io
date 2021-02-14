844fbb59b643d0e407b215f82b7fbb7f23e5b2c5
Noman1170 <77617625+Noman1170@users.noreply.github.com>
2021-02-14 03:39:10 -0800

    js basic 1st

---
+++ extra/practice.js
@@ -0,0 +1,15 @@
+//this is nbict lab's code
+
+/******************* 
+let js ="awesome";
+console .log(js);
+console.log(7-11+19*2);
+***********************/
+
+let firstname='ab';
+let lastname='noman';
+console .log(firstname+' '+lastname+'is an IT instructor and entrepreneur.'
++firstname +' founded nbict lab in 2018');
+
+let x =null ;
+console .log(typeof(x));
