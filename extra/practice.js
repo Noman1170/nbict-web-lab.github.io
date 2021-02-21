2b1be073ae01b5c3da22070e43606cd7c6107e4b
Noman1170 <77617625+Noman1170@users.noreply.github.com>
2021-02-21 03:48:19 -0800

    js

--- extra/practice.js
+++ extra/practice.js
@@ -13,3 +13,46 @@ console .log(firstname+' '+lastname+'is an IT instructor and entrepreneur.'
 
 let x =null ;
 console .log(typeof(x));
+
+
+let counttry='bangladesgh';
+let continent='asia';
+let population= 180;
+
+console.log(counttry)
+console.log(continent)
+console.log(population)
+
+let isIland= true;
+let language;
+console.log(typeof(isIland))
+console.log(typeof(country))
+console.log(typeof(language))
+
+
+
+
+//operator precedence
+
+const now =2037
+const ageJonus =now - 1992;
+const ageSara = now - 1998;
+
+console.log(now-1992>now-1998);
+
+const y = 25-10+5;
+console.log(y);
+
+/*
+const massMark = 78;
+const heightMark= 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const bmiMark = massMark / (heightMark * heightMark);
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
+const markHeigherBMI = bmiMark > bmiJohn;
+ console.log(bmiMark, bmiJohn , markHeigherBMI);
+*/
+
+ 
