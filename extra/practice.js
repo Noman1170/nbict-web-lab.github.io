3981b23e9d9ab10bdba5cffb7e5a68bb345d1b9a
Noman1170 <77617625+Noman1170@users.noreply.github.com>
2021-02-23 03:55:50 -0800

    js 0202

--- extra/practice.js
+++ extra/practice.js
@@ -5,7 +5,7 @@ let js ="awesome";
 console .log(js);
 console.log(7-11+19*2);
 ***********************/
-
+/*
 let firstname='ab';
 let lastname='noman';
 console .log(firstname+' '+lastname+'is an IT instructor and entrepreneur.'
@@ -54,5 +54,38 @@ const bmiJohn = massJohn / (heightJohn * heightJohn);
 const markHeigherBMI = bmiMark > bmiJohn;
  console.log(bmiMark, bmiJohn , markHeigherBMI);
 */
+/*
+const firstName = 'jonas';
+const job = 'teacher';
+const birthYear = 1993;
+const currentYear = 2037;
+//i'm jonas, a 30 years old teacher (``baktic)
+/*
+const jonas = "i'm " + firstName + ',a ' + (currentYear - birthYear) + 'years old' + job +'!';
 
- 
+const jonasNew = `I'm ${firstName
+}, a ${currentYear - birthYear} years old ${job}!`;
+console.log(jonas);
+console.log(jonasNew);
+
+console.log('i am \n\ a multiline \n\ string');
+  console.log(`i am
+  a multiline
+  string...`);
+*/
+/*
+const firstName = 'abdullah';
+const lastName = 'noman';
+const topic ='javaScript';
+const institue = 'NBICT LAB';
+const firstYear = '2015';
+const lastYear = '2020';
+const noman = "i am" + firstName + lastName + "i am " 'learning' + topic + 'at' + institue + 'for' + (firstYear - lastYear )+'years' +'!';
+
+console.log(noman);
+*/
+
+console.log(`i am abdullah noman.
+i am learning javaScript at NBICT LAB
+for 5 years`);
+
